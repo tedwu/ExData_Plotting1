97c470475d397da2b58d8ca679f49030f91c9e2d
Ted Wu <ted.wu.2001@gmail.com>
2015-01-08 21:18:04 +0800

    draw a line x-y plot

--- r_files/polt2.R
+++ r_files/polt2.R
@@ -1,3 +1,4 @@
+Sys.setlocale("LC_ALL","en_US.utf8")
 fileName <- 'household_power_consumption.txt'
 
 dateFormat <- '%d/%m/%Y'
@@ -7,12 +8,14 @@ endDate <- strptime('3/2/2007', dateFormat)
 
 m1 <- read.table(fileName, sep = ';', header = TRUE, stringsAsFactors = FALSE)
 m1.sub <- subset(m1, strptime(m1$Date, dateFormat) > startDate & strptime(m1$Date, dateFormat) < endDate)
+m1.sub$Date_Time <- strptime(mapply(paste, m1.sub$Date, m1.sub$Time), mapply(paste, dateFormat, timeFormat))
 
-png('polt1.png')
-hist(as.numeric(m1.sub$Global_active_power), 
-     xlab = 'Global Active Power (kilowatts)', 
-     ylab = 'Frenqucy', 
-     col = 'red', main = 'Global Active Power'
+png('polt2.png')
+plot(x = m1.sub$Date_Time,
+     y = as.numeric(m1.sub$Global_active_power),
+     xlab = NA,
+     ylab = 'Global Active Power (kilowatts)',
+     type = 'l'
 )
 
 dev.off()
